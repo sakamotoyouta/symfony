f45f9230039fbd01833f4509b63726e42d1b58b2
坂本龍太 <sakamoto@grooweb.jp>
2016-12-16 19:06:06 +0900

    勉強途中

--- classic-symfony/src/AppBundle/Controller/BlogController.php
+++ classic-symfony/src/AppBundle/Controller/BlogController.php
@@ -6,20 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BlogController extends Controller{
 	public function latestListAction(){
-		$blogList = [
-			[
-				'targetDate' => '2015年３月１５日',
-				'title' => '東京公演レポート'
-			],
-			[
-				'targetDate' => '2015年２月８日',
-				'title' => '最近の練習風景'
-			],
-			[
-				'targetDate' => '2015年１月３日',
-				'title' => '本年もよろしくお願いいたします'
-			]
-		];
+		$em = $this->getDoctrine()->getManager();
+		$blogArticleRepository = $em->getRepository('AppBundle:BlogArticle');
+		$blogList = $blogArticleRepository->findBy([],['targetDate'=>'DESC']);
 
 		return $this->render('Blog/latestList.html.twig',['blogList' => $blogList]);
 	}
